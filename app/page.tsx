bcbe17e96003f70026568864ad07636cae661850
Lee Jackson <130007945+Imagineer99@users.noreply.github.com>
2023-12-12 19:42:15 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useChat } from 'ai/react';
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white font-bold text-lg">Neural Novel</div>
+        {/* Make the logo clickable */}
+        <a href="https://neuralnovel.com" className="text-white font-bold text-lg">
+          Neural Novel
+        </a>
+
         {/* Add additional navigation items as needed */}
-        
+
         {/* Donate button */}
         <a
           href="https://streamlabs.com/neuralnovels/tip"
@@ -28,6 +32,7 @@ export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   const [showLabel, setShowLabel] = useState(true);
   const [showTitle, setShowTitle] = useState(true);
+  const [showULA, setShowULA] = useState(false);
 
   const handleEnterPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
@@ -36,14 +41,26 @@ export default function Chat() {
     }
   };
 
+  const handleULAClick = () => {
+    setShowULA(true);
+  };
+
+  const handleULAClose = () => {
+    setShowULA(false);
+  };
+
   return (
     <div>
       {/* Include the Navbar component */}
-      
       <Navbar />
 
       {/* Rest of your Chat component */}
       <div className="mx-auto w-full max-w-md py-24 flex flex-col items-center">
+        {/* User License Agreement Link */}
+        <a href="#" onClick={handleULAClick} className="DG">
+          User License Agreement
+        </a>
+
         <h1
           style={{
             opacity: showTitle ? 1 : 0,
@@ -55,7 +72,8 @@ export default function Chat() {
         >
           Mixtral Chat
         </h1>
-        
+
+        {/* Messages display */}
         {messages.map((m) => (
           <div key={m.id}>
             <span style={{ fontWeight: 'bold' }}>
@@ -65,6 +83,7 @@ export default function Chat() {
           </div>
         ))}
 
+        {/* User input form */}
         <form onSubmit={handleSubmit}>
           <label>
             <span
@@ -75,7 +94,7 @@ export default function Chat() {
               Say something...
             </span>
             <input
-              className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2"
+              className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2 md:mx-auto md:w-1/2 lg:w-1/3"
               value={input}
               onChange={handleInputChange}
               onKeyDown={handleEnterPress}
@@ -83,6 +102,25 @@ export default function Chat() {
           </label>
           <button type="submit">Send</button>
         </form>
+
+        {/* User License Agreement Modal */}
+        {showULA && (
+          <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
+            <div className="bg-white p-8 rounded shadow-lg">
+              <h2 className="text-lg font-bold mb-4">User License Agreement</h2>
+              <p>
+                By using this website and its components, you agree that it is for testing purposes only.
+                You are not allowed Use this website for any illegal purpose, or in violation of any laws. 
+                By using this website you agree to our terms & conditions.
+              </p>
+              <div className="text-center mt-4">
+                <button onClick={handleULAClose} className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer">
+                  OK
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
