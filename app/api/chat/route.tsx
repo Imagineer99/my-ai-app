260396e8e676c9dfa3500e661112fb2a3866a962
Lee Jackson <130007945+Imagineer99@users.noreply.github.com>
2023-12-13 02:09:18 +0000

    Update route.tsx

--- app/api/chat/route.tsx
+++ app/api/chat/route.tsx
@@ -4,7 +4,7 @@ import { OpenAIStream, StreamingTextResponse } from 'ai';
 // Create an OpenAI API client (that's edge friendly!)
 // but configure it to point to fireworks.ai
 const fireworks = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY || '',
+  apiKey: process.env.OPENAI_API_KEY || '',,
   baseURL: 'https://api.fireworks.ai/inference/v1',
 });
 // IMPORTANT! Set the runtime to edge
@@ -16,13 +16,13 @@ export async function POST(req: Request) {
   // Ask Fireworks for a streaming chat completion using Llama 2 70b model
   // @see https://app.fireworks.ai/models/fireworks/llama-v2-70b-chat
   const response = await fireworks.chat.completions.create({
-    model: 'accounts/fireworks/models/llama-v2-7b-chat',
+    model: 'accounts/fireworks/models/llama-v2-70b-chat',
     stream: true,
     max_tokens: 1000,
         messages: [
       {
         "role": "user",
-        "content" : "You are Mixtral, Developed by Mistral.AI"
+        "content" : "You are Mixtral MoE 8x7B, Developed by Mistral.AI"
       }
     ],
   });
@@ -30,3 +30,4 @@ export async function POST(req: Request) {
   const stream = OpenAIStream(response);
   // Respond with the stream
   return new StreamingTextResponse(stream);
+}
